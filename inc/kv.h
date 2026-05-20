8a03367c20b9b9c1e45bfdee5e4c3110e1735285
Allan Hillyer <athillyer@profben.com>
2026-05-20 11:48:05 -0700

    update kv.h

---
+++ inc/kv.h
@@ -0,0 +1,28 @@
+#ifndef KV_H
+#define KV_H
+
+#include <stdlib.h>
+
+#define TOMBSTONE ((char *)0x1)
+
+typedef struct
+{
+    char *key;
+    char *value;
+} kv_entry_t;
+
+typedef struct 
+{
+    size_t capacity;
+    size_t count;
+    kv_entry_t *entries;
+
+} kv_t;
+
+kv_t *kv_init(size_t capacity);
+int    kv_put(kv_t *db, const char *key, const char *value);
+char  *kv_get(kv_t *db, const char *key);
+int    kv_delete(kv_t *db, const char *key);
+void   kv_free(kv_t *db);
+
+#endif
